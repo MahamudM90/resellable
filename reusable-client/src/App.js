638d62a4070627daebb74f8d892ac39093c57b8c
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-12-04 11:53:51 +0600

    first commit

---
+++ reusable-client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from './Router/Routes/Routes';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <div data-theme='lofi'>
+      <RouterProvider router={router} />
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
